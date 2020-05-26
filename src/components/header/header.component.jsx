ccb39bea084147969141e249400bdcc606e3ae83
Sanjay Sharma <sanjaysharma@Sanjays-MacBook-Pro-2.local>
2020-05-25 20:08:42 -0400

    implemented reducer and added usermap

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect} from 'react-redux';
 
 
 import { Link } from 'react-router-dom';
@@ -33,4 +34,7 @@ const Header = ({ currentUser }) => (
     </header>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+export default connect(mapStateToProps) (Header);
